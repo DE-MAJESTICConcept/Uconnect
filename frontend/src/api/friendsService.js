724168d5a8707f42b79bf4a8ffc9d1a9a8152724
DE-MAJESTICConcept <149531935+DE-MAJESTICConcept@users.noreply.github.com>
2025-10-04 02:51:26 +1300

    Update friendsService.js

--- frontend/src/api/friendsService.js
+++ frontend/src/api/friendsService.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://uconnect-backend-2qnn.onrender.com/api",
+  baseURL: "https://uconnect-backend-2qnn.onrender.com/api/friends",
 });
 
 // attach token
