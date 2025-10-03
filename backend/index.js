ebfc2c14953565af8d7b1658ca8bcf1a055e890e
DE-MAJESTICConcept <149531935+DE-MAJESTICConcept@users.noreply.github.com>
2025-10-04 01:45:29 +1300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -44,13 +44,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRoutes);
 app.use("/messages", messageRoute); // <-- mount message routes at /api/messages
 app.use("/admin", adminRoutes);
-app.use("/api/profile", profileRoutes);
-app.use("/api/friends", friendsRoutes);
-app.use("/api/posts", postsRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/styles", stylesRoutes);
-app.use("/api/items", itemRoutes);
-app.use("/api/student-learning", studentLearningRoutes);
+app.use("/profile", profileRoutes);
+app.use("/friends", friendsRoutes);
+app.use("/posts", postsRoutes);
+app.use("/users", userRoutes);
+app.use("/styles", stylesRoutes);
+app.use("/items", itemRoutes);
+app.use("/student-learning", studentLearningRoutes);
 
 
 
