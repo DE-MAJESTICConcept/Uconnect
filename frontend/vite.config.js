ae58f15149ce34d3600b81d75faa3edce68874d0
DE-MAJESTICConcept <149531935+DE-MAJESTICConcept@users.noreply.github.com>
2025-10-10 23:39:06 +1300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-})
-// vite.config.js
-export default {
   build: {
     chunkSizeWarningLimit: 1600, // in KB (1.6 MB)
   },
-};
+})
+
